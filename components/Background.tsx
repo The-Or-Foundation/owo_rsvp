ed0ef322789fa37027286e4e0e6fa85c4a26ff11
the-or-web <web@theor.org>
2025-08-11 11:33:27 +0000

    chore: 🎯 first shot

---
+++ components/Background.tsx
@@ -0,0 +1,7 @@
+// components/Background.tsx
+import React from "react";
+import styles from "./Background.module.css";
+
+export default function Background() {
+  return <div className={styles.background}></div>;
+}
